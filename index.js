99c674ce0d51b6a3334b6403b9e67b6707579eac
Tamim-real <tamimislam2653@gmail.com>
2025-12-22 20:50:48 +0600

    unfavorating api integrated

--- index.js
+++ index.js
@@ -354,6 +354,15 @@ async function run() {
             res.send(result)
         })
 
+        app.delete('/favorites/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+
+            const result = await favCollection.deleteOne(query);
+            res.send(result)
+            
+        })
+
         app.get('/', (req, res) => {
             res.send('Hello w')
         })

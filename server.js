e8c62649a14330fed7afe6913150e41e3621c154
Tamim-real <tamimislam2653@gmail.com>
2026-01-30 19:29:33 +0600

    some minor issues fixed

--- server.js
+++ server.js
@@ -2,16 +2,16 @@ const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
-const connectDB = require('./config/db');
+const connectDB = require('./src/config/db');
 
 
-const userRoutes = require('./routes/userRoutes');
-const mealRoutes = require('./routes/mealRoutes');
-const roleRoutes = require('./routes/roleRequestRoutes');
-const orderRoutes = require('./routes/orderRoutes');
-const paymentRoutes = require('./routes/paymentRoutes');
-const reviewRoutes = require('./routes/reviewRoutes');
-const favoriteRoutes = require('./routes/favoriteRoutes');
+const userRoutes = require('./src/routes/userRoutes');
+const mealRoutes = require('./src/routes/mealRoutes');
+const roleRoutes = require('./src/routes/roleRequestRoutes');
+const orderRoutes = require('./src/routes/orderRoutes');
+const paymentRoutes = require('./src/routes/paymentRoutes');
+const reviewRoutes = require('./src/routes/reviewRoutes');
+const favoriteRoutes = require('./src/routes/favoriteRoutes');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -24,7 +24,7 @@ async function run() {
     try {
         const db = await connectDB();
 
-        
+       
         const usersCollection = db.collection("usersCollection");
         const mealsCollection = db.collection("mealsCollection");
         const requestsCollection = db.collection("roleRequests");
@@ -32,15 +32,14 @@ async function run() {
         const reviewCollection = db.collection("reviewCollections");
         const favCollection = db.collection("favCollections");
 
-        // --- Auth / JWT API ---
+        
         app.post('/jwt', async (req, res) => {
             const user = req.body;
             const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: '1h' });
             res.send({ token });
         });
 
-        // --- Routes Setup ---
-        // আমরা প্রত্যেকটি রাউট ফাইলে প্রয়োজনীয় কালেকশনগুলো পাস করে দিচ্ছি
+       
         app.use('/', userRoutes(usersCollection));
         app.use('/', mealRoutes(mealsCollection));
         app.use('/api', roleRoutes(requestsCollection, usersCollection));
@@ -49,7 +48,7 @@ async function run() {
         app.use('/', reviewRoutes(reviewCollection));
         app.use('/', favoriteRoutes(favCollection));
 
-     
+        // Default Route
         app.get('/', (req, res) => {
             res.send('Fudex Server is running...');
         });

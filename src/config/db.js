3bab3b9099f70448822de07892b905875305793d
Tamim-real <tamimislam2653@gmail.com>
2026-01-30 18:43:33 +0600

    db file updated

---
+++ src/config/db.js
@@ -0,0 +1,18 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const uri = process.env.MONGO_URI;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+const connectDB = async () => {
+  await client.connect();
+  console.log("MongoDB Connected");
+};
+
+module.exports = { client, connectDB };
